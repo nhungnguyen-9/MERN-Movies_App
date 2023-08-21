353bddb17eafffcdb66462c0670fdbd35e7e0966
NhungNguyen-9 <nguyennhung9846@gmail.com>
2023-08-21 14:01:42 +0700

    v.1

---
+++ server/src/controllers/favorite.controller.js
@@ -0,0 +1,50 @@
+import responseHandler from "../handlers/response.handler.js";
+import favoriteModel from "../models/favorite.model.js";
+
+const addFavorite = async (req, res) => {
+    try{
+        const isFavorite = await favoriteModel.findOne({user: req.user.id, movieId: req.body.mediaId});
+        
+        if(isFavorite) return responseHandler.ok(res, isFavorite);
+
+        const favorite = new favoriteModel({
+            ...req.body,
+            user: req.user.id
+        });
+
+        await favorite.save();
+
+        responseHandler.created(res, favorite);
+    
+    }catch{
+        responseHandler.error(res);
+    }
+};
+
+const removeFavorite = async (req, res) => {
+    try{
+        const { favoriteId }= req.params;
+
+        const favorite = await favoriteModel.findOne({ _id: favoriteId, user: req.user.id});
+
+        if(!favorite) return responseHandler.notFound(res);
+
+        await favorite.remove();
+
+        responseHandler.ok(res);
+    }catch{
+        responseHandler.error(res);
+    }
+};
+
+const getFavoritesOfUser = async (req, res) => {
+    try{
+        const favorite = await favoriteModel.find({user: req.user.id}).sort("-createdAt");
+
+        responseHandler.ok(res, favorite);
+    }catch{
+        responseHandler.error(res);
+    }
+};
+
+export default {addFavorite, removeFavorite, getFavoritesOfUser};
